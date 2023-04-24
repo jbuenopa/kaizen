36e6bd27c8648af6d3320dbab752fdffecc0d8fb
miguicap <miguel-angel.orozco-tortosa.external@capgemini.com>
2023-04-24 14:44:56 +0200

    Proyecto Kaizen Malaga

---
+++ CapFashion/infra/outputs.tf
@@ -0,0 +1,34 @@
+# Azure AKS Outputs
+
+output "aks_cluster_id" {
+  value = azurerm_kubernetes_cluster.aks_cluster.id
+}
+
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks_cluster.name
+}
+
+output "aks_cluster_kubernetes_version" {
+  value = azurerm_kubernetes_cluster.aks_cluster.kubernetes_version
+}
+
+# Resource Group Outputs
+output "location" {
+  value = azurerm_resource_group.aks_rg.location
+}
+
+output "resource_group_id" {
+  value = azurerm_resource_group.aks_rg.id
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.aks_rg.name
+}
+
+output "azurerm_public_ip" {
+  value = azurerm_public_ip.public_ip1.id
+}
+
+output "azurerm_public_ip2" {
+  value = azurerm_public_ip.public_ip2.id
+}
---
+++ CapFashion/infra/vars.tf
@@ -0,0 +1,29 @@
+# Azure Location
+variable "location" {
+  type = string
+  description = "Azure Region where all these resources will be provisioned"
+  default = "West Europe"
+}
+
+# Azure Resource Group Name
+variable "resource_group_name" {
+  type = string
+  description = "This variable defines the Resource Group"
+  default = "Proyecto-Kaizen-2023"
+}
+
+# Azure AKS Environment Name
+variable "environment" {
+  type = string  
+  description = "This variable defines the Environment"  
+  default = "proyecto"
+}
+
+
+# # AKS Input Variables
+
+# # SSH Public Key for Linux VMs
+# variable "ssh_public_key" {
+#   default = "~/.ssh/aks-prod-sshkeys-terraform/aksprodsshkey.pub"
+#   description = "This variable defines the SSH Public Key for Linux k8s Worker nodes"  
+# }
